f079aa23ab5b3a7d2030923d51241d2532ecfd17
Marti Sabaté <71668554+msabate00@users.noreply.github.com>
2024-04-14 18:36:18 +0200

    MSM - TP Entre salas terminado

--- project/Game/Source/TPEntity.cpp
+++ project/Game/Source/TPEntity.cpp
@@ -48,6 +48,27 @@ void TPEntity::OnCollision(PhysBody* physA, PhysBody* physB) {
 	{
 		case ColliderType::PLAYER:
 			LOG("Player se mueve a: X: %d, Y: %d", targetPosition.x, targetPosition.y);
+			iPoint targetPos;
+
+			int difX = abs(targetPosition.x - position.x);
+			int difY = abs(targetPosition.y - position.y);
+
+			if (difY > difX) {
+				if (targetPosition.y < position.y) { targetPos.y = targetPosition.y - 64; }
+				else { targetPos.y = targetPosition.y + 64; }
+				targetPos.x = targetPosition.x;
+			}
+			else {
+				if (targetPosition.x < position.x) { targetPos.x = targetPosition.x - 64; }
+				else { targetPos.x = targetPosition.x + 64; }
+				targetPos.y = targetPosition.y;
+			}
+
+			
+
+			
+
+			app->entityManager->GetPlayer()->pbody->body->SetTransform(b2Vec2(PIXEL_TO_METERS(targetPos.x),PIXEL_TO_METERS(targetPos.y)), 0);
 			break;
 	}
 }
