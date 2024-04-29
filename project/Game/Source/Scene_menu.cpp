3395678e740c04161fad56947f8578d104c7d304
CaseChips <ncasemasana04@gmail.com>
2024-04-29 19:26:51 +0200

    NCM - créditos

--- project/Game/Source/Scene_menu.cpp
+++ project/Game/Source/Scene_menu.cpp
@@ -507,7 +507,7 @@ void Scene_Menu::ShowCredits()
 			control->data->state = GuiControlState::DISABLED; 
 		}
 
-		gcCloseCredits = app->guiManager->CreateGuiControl(GuiControlType::BUTTON, 12, "ATR�S", SDL_Rect{ (int)windowW / 2 - 68,	(int)windowH - 100,	136,46 }, this);
+		gcCloseCredits = app->guiManager->CreateGuiControl(GuiControlType::BUTTON, 12, "ATRÁS", SDL_Rect{ (int)windowW / 2 - 68,	(int)windowH - 100,	136,46 }, this);
 		_showCredits = true;
 	}
 
