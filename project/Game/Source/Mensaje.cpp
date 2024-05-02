8919ccd85186d1a5cecfb44827fbf9816320a4b6
Aleix3 <Aleixbotella.1@gmail.com>
2024-05-02 20:58:03 +0200

    mensaje inicio

---
+++ project/Game/Source/Mensaje.cpp
@@ -0,0 +1,70 @@
+#include "Mensaje.h"
+#include "App.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Input.h"
+#include "Render.h"
+#include "Scene_testing.h"
+#include "Log.h"
+#include "Point.h"
+#include "Physics.h"
+
+Mensaje::Mensaje(EntityType type, int id, int ataque, int durabilidad, int magia, float peso)
+	: type(type), ataque(ataque), durabilidad(durabilidad), magia(magia), peso(peso), Entity(EntityType::MENSAJE)
+{
+	name.Create("mensaje");
+}
+
+Mensaje::~Mensaje() {}
+
+bool Mensaje::Awake() {
+
+	
+	 
+
+	return true;
+}
+
+bool Mensaje::Start() {
+	
+	//initilize textures
+	/*position.x = parameters.attribute("x").as_int();
+	position.y = parameters.attribute("y").as_int();*/
+	texture = app->tex->Load(config.attribute("texturePath").as_string());
+	description = (config.attribute("description").as_string());
+	tipo = (config.attribute("type").as_string());
+	/*texture = app->tex->Load("Assets/Textures/Entidades/Items/item_Garra.png");*/
+	// L07 DONE 4: Add a physics to an item - initialize the physics body
+	app->tex->GetSize(texture, texW, texH);
+	
+
+	return true;
+}
+
+bool Mensaje::Update(float dt)
+{
+	// L07 DONE 4: Add a physics to an item - update the position of the object from the physics.  
+
+	b2Transform pbodyPos = pbody->body->GetTransform();
+	position.x = METERS_TO_PIXELS(pbodyPos.p.x) - 11;
+	position.y = METERS_TO_PIXELS(pbodyPos.p.y) - 11;
+
+	
+	
+	return true;
+}
+
+bool Mensaje::PostUpdate()
+{
+	app->render->DrawTexture(texture, 980, 550, SDL_FLIP_NONE, 0, 0);
+	return true;
+}
+
+bool Mensaje::CleanUp()
+{
+	app->physics->GetWorld()->DestroyBody(pbody->body);
+	app->tex->UnLoad(texture);
+	return true;
+}
+
+
