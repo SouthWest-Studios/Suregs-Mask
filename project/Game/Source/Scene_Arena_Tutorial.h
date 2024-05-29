5a90921dd4610b8794a3e1754dc08f4673f8124e
Cifu2004 <albertitocibu@gmail.com>
2024-05-29 19:50:40 +0200

    NCM - Scene arena tutorial

---
+++ project/Game/Source/Scene_Arena_Tutorial.h
@@ -0,0 +1,66 @@
+#ifndef __Scene_Arena_Tutorial_H__
+#define __Scene_Arena_Tutorial_H__
+
+#include "Module.h"
+#include "Player.h"
+#include "GuiControl.h"
+#include "GuiControlButton.h"
+#include "Particle.h"
+#include "ParticlePool.h"
+#include "Emitter.h"
+
+
+struct SDL_Texture;
+
+class Scene_Arena_Tutorial : public Module
+{
+public:
+
+	Scene_Arena_Tutorial(App* app, bool start_enabled = false);
+
+	// Destructor
+	virtual ~Scene_Arena_Tutorial();
+
+	// Called before render is available
+	bool Awake(pugi::xml_node config);
+
+	// Called before the first frame
+	bool Start();
+
+	// Called before all Updates
+	bool PreUpdate();
+
+	// Called each loop iteration
+	bool Update(float dt);
+
+	// Called before all Updates
+	bool PostUpdate();
+
+	// Called before quitting
+	bool CleanUp();
+
+	// Handles multiple Gui Event methods
+	bool OnGuiMouseClickEvent(GuiControl* control);
+
+private:
+
+	float textPosX, textPosY = 0;
+	uint texW, texH;
+	uint windowW, windowH;
+	SDL_Texture* mouseTileTex = nullptr;
+
+	int town_fx;
+	
+
+	//L03: DONE 3b: Declare a Player attribute
+	
+
+	// L15: TODO 2: Declare a GUI Control Button 
+	GuiControlButton* gcButtom;
+
+public:
+
+	PhysBody* pbody;
+};
+
+#endif // __Scene_Arena_Tutorial_H__
