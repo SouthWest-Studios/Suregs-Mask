067683136d40967e83c3563e15403ef19324a0dd
AlejandroIR <shibainuale@gmail.com>
2024-06-06 21:58:38 +0200

    AIR - 🍭Borook fix🍭

--- project/Game/Source/EntityManager.cpp
+++ project/Game/Source/EntityManager.cpp
@@ -523,7 +523,7 @@ std::vector<Entity*> EntityManager::GetEnemies() {
 	std::vector<Entity*> enemies;
 	for (ListItem<Entity*>* item = entities.start; item != NULL; item = item->next) {
 		Entity* entity = item->data;
-		if (entity->type == EntityType::ENEMY_OSIRIS || entity->type == EntityType::ENEMY_OSIRIS_VARIATION || entity->type == EntityType::ENEMY_OLS || entity->type == EntityType::ENEMY_OLS_VARIATION || entity->type == EntityType::ENEMY_SHAR || entity->type == EntityType::ENEMY_KHURT ||
+		if (entity->type == EntityType::ENEMY_OSIRIS || entity->type == EntityType::ENEMY_OSIRIS_VARIATION || entity->type == EntityType::ENEMY_OLS || entity->type == EntityType::ENEMY_OLS_VARIATION || entity->type == EntityType::ENEMY_SHAR || entity->type == EntityType::ENEMY_KHURT || entity->type == EntityType::ENEMY_KHURT_VARIATION || entity->type == EntityType::ENEMY_BOOROK || entity->type == EntityType::ENEMY_BOOROK_VARIATION ||
 			entity->type == EntityType::BOSS_INUIT || entity->type == EntityType::BOSS_MUSRI || entity->type == EntityType::BOSS_SURMA ||
 			entity->type == EntityType::BOSS_IGORY) {
 			enemies.push_back(entity);
