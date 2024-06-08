9e9448822fb6efe86108e9d50b2f8c3c671ec4e0
Galycon <2004rasr@gmail.com>
2024-06-09 01:40:47 +0200

    RSR - Misiones feas de pelotas pero se ven a tomar por culo

--- project/Game/Source/Render.cpp
+++ project/Game/Source/Render.cpp
@@ -62,8 +62,8 @@ bool Render::Awake(pugi::xml_node config)
 	//load a font into memory
 	primaryFont = TTF_OpenFont("Assets/Fuentes/BAHNSCHRIFT.ttf", 25);
 	titleFont = TTF_OpenFont("Assets/Fuentes/Timeless-Bold.ttf", 25);
-	goldFont = TTF_OpenFont("Assets/Fuentes/Timeless-Bold.ttf", 16);
-	questFont = TTF_OpenFont("Assets/Fuentes/Timeless-Bold.ttf", 20);
+	goldFont = TTF_OpenFont("Assets/Fuentes/Timeless-Bold.ttf", 18);
+	questFont = TTF_OpenFont("Assets/Fuentes/Timeless-Bold.ttf", 14);
 	consoleFont = TTF_OpenFont("Assets/Fuentes/UbuntuMono-R.ttf", 25);
 	buttonFont = TTF_OpenFont("Assets/Fuentes/BAHNSCHRIFT.ttf", 25);
 
